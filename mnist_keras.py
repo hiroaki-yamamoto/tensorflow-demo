9573a243223564b2b7399473fc8ef3e837117964
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2017-02-23 21:32:30 +0900

    Trying to understand keras

---
+++ mnist_keras.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""Mnist CNN based on keras' example."""
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+np_class = 10  # That is the number of integers from 0 to 9 = 10
